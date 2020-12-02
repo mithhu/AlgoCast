d2e72f12794121550f52ab2611709469261b8d23
mithhu <mahamudul.hasan@evaly.com.bd>
2020-12-03 00:16:30 +0600

    slice method

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,31 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunkedArray = [];
+  let index = 0;
+  while (index < array.length) {
+    chunkedArray.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunkedArray;
+}
 
 module.exports = chunk;
+
+//Sol-1
+// function chunk(array, size) {
+//   let chunkedArray = [];
+//   for (let elem of array) {
+//     const currentArray = chunkedArray[chunkedArray.length - 1];
+//     if (!currentArray || currentArray.length === size) {
+//       chunkedArray.push([elem]);
+//     } else {
+//       currentArray.push(elem);
+//     }
+//   }
+//   return chunkedArray;
+// }
+
+// module.exports = chunk;
